b7abe0f1bb176d94baad08ab1ceffdc0d626742a
Boram-Hwang <95403102+Boram-Hwang@users.noreply.github.com>
2024-04-29 11:03:21 +0900

    Title: 4가지 관계연산자, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

--- 240429/4가지 관계연산자/four-relational-operators.java
+++ 240429/4가지 관계연산자/four-relational-operators.java
@@ -7,25 +7,38 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        if(a >= b) {
-            System.out.println(1);
-        }else {
-            System.out.println(0);
-        }
+        intValue(a >= b);
+        intValue(a > b);
+        intValue(a <= b);
+        intValue(a < b);
 
-        if(a > b) {
-            System.out.println(1);
-        }else {
-            System.out.println(0);
-        }
+        // if(a >= b) {
+        //     System.out.println(1);
+        // }else {
+        //     System.out.println(0);
+        // }
 
-        if(a <= b) {
-            System.out.println(1);
-        }else {
-            System.out.println(0);
-        }
+        // if(a > b) {
+        //     System.out.println(1);
+        // }else {
+        //     System.out.println(0);
+        // }
 
-        if(a < b) {
+        // if(a <= b) {
+        //     System.out.println(1);
+        // }else {
+        //     System.out.println(0);
+        // }
+
+        // if(a < b) {
+        //     System.out.println(1);
+        // }else {
+        //     System.out.println(0);
+        // }
+    }
+
+    public static void intValue(boolean value) {
+        if(value == true) {
             System.out.println(1);
         }else {
             System.out.println(0);
