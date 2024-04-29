633eda7863a9ca45f14404f56870a51d47ca70ce
Boram-Hwang <95403102+Boram-Hwang@users.noreply.github.com>
2024-04-29 16:00:05 +0900

    Title: 장학금, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240429/장학금/scholarship.java
+++ 240429/장학금/scholarship.java
@@ -6,13 +6,11 @@ public class Main {
         int s = sc.nextInt();
         int s1 = sc.nextInt();
 
-        if(s >= 90 && s1 >= 90) {
-            if(s >= 95 && s1 >= 95){
-                System.out.println(100000);
-            } else if (s>=90&&s1>=90){
-                System.out.println(50000);
-            }
-        }else{
+        if(s >= 90 && s1 >= 95) {
+            System.out.println(100000);
+        } else if (s>=90&&s1>=90){
+            System.out.println(50000);
+        }else {
             System.out.println(0);
         }
     }
