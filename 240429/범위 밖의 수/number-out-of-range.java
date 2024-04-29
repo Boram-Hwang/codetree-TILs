3c0d8c75d0474d80a91a53a6ef347a6aaebdd18c
Boram-Hwang <95403102+Boram-Hwang@users.noreply.github.com>
2024-04-29 14:20:47 +0900

    Title: 범위 밖의 수, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240429/범위 밖의 수/number-out-of-range.java
+++ 240429/범위 밖의 수/number-out-of-range.java
@@ -6,7 +6,7 @@ public class Main {
 
         int a = sc.nextInt();
 
-        if(a <= 10 || a > 20) {
+        if(a < 10 || a > 20) {
             System.out.println("yes");
         } else {
             System.out.println("no");
