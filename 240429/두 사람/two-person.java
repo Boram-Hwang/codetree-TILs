0511d60bea417c15a0cf12633df3b589170660bc
Boram-Hwang <95403102+Boram-Hwang@users.noreply.github.com>
2024-04-29 16:13:41 +0900

    Title: 두 사람, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240429/두 사람/two-person.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a_age = sc.nextInt();
+        String a_gen = sc.next();
+        int b_age = sc.nextInt();
+        String b_gen = sc.next();
+
+        if(a_age >= 19 && a_gen.equals("M") || b_age >= 19 && b_gen.equals("M")) {
+            System.out.println(1);
+        }else {
+            System.out.println(0);
+        }
+    }
+}
