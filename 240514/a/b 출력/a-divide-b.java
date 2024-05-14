26e1723d84ec43fb128d34d2e9b320b44a40a035
Boram-Hwang <95403102+Boram-Hwang@users.noreply.github.com>
2024-05-14 16:40:04 +0900

    Title: a/b 출력, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240514/a/b 출력/a-divide-b.java
+++ 240514/a/b 출력/a-divide-b.java
@@ -7,8 +7,23 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         
-        double result = (double)a/b;
-        String formattedResult = String.format("%.20f",result);
-        System.out.printf(formattedResult);
+        // double result = (double)a/b;
+        // String formattedResult = String.format("%.20f",result);
+        // System.out.printf(formattedResult);
+        // 정수부분 출력
+        System.out.print(a/b + ".");
+        
+        // a를 b로 나눈 나머지를 시작으로
+        // 소수점 자리를 하나씩 계산
+        a %= b;
+        for(int i = 0; i< 20; i++) {
+            // 나머지에 10 곱한 값을 기준으로
+            // b로 나누었을 떄의 몫을 구해줌
+            a *= 10;
+            System.out.print(a/b);
+
+            // a를 b로 나눈 나머지를 게속 갱신
+            a %= b;
+        }
     }
 }
