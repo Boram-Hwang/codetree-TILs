b4d795c62d51e153ba95f42297d91c2a3cc28c0b
Boram-Hwang <95403102+Boram-Hwang@users.noreply.github.com>
2024-05-20 15:49:34 +0900

    Title: 숫자의 증감, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240520/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        char c = a.charAt(0);
+        int n = sc.nextInt();
+
+        if(c == 'A'){
+            for(int i = 1; i <= n; i++) {
+                System.out.print(i + " ");
+            }
+        }else if(c == 'D') {
+            for(int i = 1; i >= n; i--) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
